771e98fe698f6dd8e792368915094e9827cc1f82
Daniel García Tovar <danigt91@gmail.com>
2016-02-24 23:19:26 +0100

    Restructure Scripts & Content

--- VideoStreaming/VideoStreaming/App_Start/BundleConfig.cs
+++ VideoStreaming/VideoStreaming/App_Start/BundleConfig.cs
@@ -9,20 +9,24 @@ namespace VideoStreaming
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/libs/jquery/jquery-{version}.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Scripts/libs/modernizr/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/libs/bootstrap/bootstrap.js",
+                      "~/Scripts/libs/respond/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/lib/bootstrap/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                "~/Scripts/libs/angular/angular.js"
+                ));
         }
     }
 }
--- VideoStreaming/VideoStreaming/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace VideoStreaming.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
